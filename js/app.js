9d30410fd9ae88e267af240746ca71bbc30e35d1
robertabrooks <robertb1@uw.edu>
2014-11-18 11:06:23 -0800

    demo

--- js/app.js
+++ js/app.js
@@ -6,3 +6,21 @@
 // lat: 47.655
 // lng: -122.3080
 
+$(document).ready(function() {
+    var mapElem = document.getElementById('map');
+
+    function createMap(elem, center, zoom) {
+        var map = new google.maps.Map(elem, {
+            center: center,
+            zoom: zoom,
+            mapTypeId: google.maps.MapTypeId.SATELLITE
+        });
+    }
+
+    var center = {
+        lat: 47.655,
+        lng: -122.3080
+    };
+
+    createMap(mapElem, center, 14);
+}); //doc ready
